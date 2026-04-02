9b30af5322b25e0d89ab76c52a7f4bdbda30ba8b
Nemanja <nemanjapet94@gmail.com>
2026-04-02 19:02:28 +0200

    Version update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.neckosstudio.gymtracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.0.2"
     }
 
     buildTypes {
